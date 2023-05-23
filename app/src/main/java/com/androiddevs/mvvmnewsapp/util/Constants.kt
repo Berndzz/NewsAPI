e3afed6ec75fbc9c337ad6760a2c8603d550159d
Berndzz <tukanbernad@gmail.com>
2023-05-24 06:06:09 +0700

    setup api interface, retrofit, and util package

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.androiddevs.mvvmnewsapp.util
+
+// https://newsapi.org/v2/top-headlines
+// https://newsapi.org/v2/everything
+
+class Constants {
+    companion object{
+        const val API_KEY = "9e77238f14f84361ba840eca28f4ac40"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
